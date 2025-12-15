68645d6309eebfe73d1db91e038f7ba5e2cab29f
kelmo <kelvmod@gmail.com>
2025-12-15 21:25:07 +1000

    Execute fll scripts during mount phase of dracut init.

--- dracut/modules.d/70fll/module-setup.sh
+++ dracut/modules.d/70fll/module-setup.sh
@@ -21,7 +21,7 @@ install() {
         mkdir mount readlink rmdir sed stat umount \
         fll_blockdev_detect
     inst_simple /etc/default/distro
-    inst_hook pre-udev 30 "$moddir/fll.sh"
+    inst_hook mount 00 "$moddir/fll.sh"
     inst_script "$moddir/fll-udev.sh" "/sbin/fll-udev"
     inst_script "/usr/share/fll-live-initramfs/fll.initramfs" "/sbin/fll-live-root"
 }
