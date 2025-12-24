fab9e0b861db7ba2f923866c1520b1755bb02495
kelmo <kelvmod@gmail.com>
2025-12-24 11:22:41 +1000

    Move initramfs-tools infra into subfolder of same name and adjust debian/rules.
    * Move initramfs-tools infra into subfolder of same name and adjust
      debian/rules.
    * Add the following modules explicitly to dracut initramfs:
      - overlay (required for live cow overlay)
      - of_pmem nd_pmem nfit (required for UEFI https boot)
      - change ntfs to ntfs3
    * Add utilities to dracut initramfs:
      - eject (for legacy ejection of physical cd/dvd media)
      - systemd-detect-virt (to avoid ejection in virt envs)
    * Add fll.shutdown to exit-initramfs in dracut initramfs.
    * Adapt initramfs-tools eject logic so it may be shared with dracut
      initramfs unconditionally.

--- dracut/modules.d/70fll/module-setup.sh
+++ dracut/modules.d/70fll/module-setup.sh
@@ -11,15 +11,18 @@ depends() {
 }
 
 installkernel() {
-    hostonly='' instmods iso9660 erofs loop squashfs \
-        ext4 btrfs jfs f2fs xfs ntfs vfat exfat udf
+    hostonly='' instmods iso9660 erofs loop squashfs overlay \
+        ext4 btrfs jfs f2fs xfs ntfs3 vfat exfat udf \
+        of_pmem nd_pmem nfit
 }
 
 install() {
-    inst_multiple awk blkid blockdev cat dd echo grep losetup \
-        mkdir mount readlink rmdir sed stat umount \
-        fll_blockdev_detect
+    inst_multiple awk blkid blockdev cat dd echo eject grep \
+        losetup mkdir mount readlink rmdir sed stat umount \
+        fll_blockdev_detect systemd-detect-virt
     inst_simple /etc/default/distro
     inst_hook mount 99 "$moddir/fll.sh"
     inst_script "/usr/share/fll-live-initramfs/fll.initramfs" "/sbin/fll"
+    inst_script "/usr/share/fll-live-initramfs/fll.shutdown" \
+        "/usr/lib/systemd/system-shutdown/fll"
 }
